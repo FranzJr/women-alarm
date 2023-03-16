de95092db1b6234f65f33164c6647e70d040c56c
Franz Joseph <frogelez@zenware.com.co>
2023-03-15 20:56:55 -0500

    Menu stylesheet

--- src/app/pages/session-detail/session-detail.ts
+++ src/app/pages/session-detail/session-detail.ts
@@ -44,7 +44,7 @@ export class SessionDetailPage {
   }
 
   ionViewDidEnter() {
-    this.defaultHref = `/app/tabs/schedule`;
+    this.defaultHref = `/login`;
   }
 
   sessionClick(item: string) {
--- src/app/pages/tabs-page/tabs-page-routing.module.ts
+++ src/app/pages/tabs-page/tabs-page-routing.module.ts
@@ -59,7 +59,7 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/app/tabs/schedule',
+        redirectTo: '/login',
         pathMatch: 'full'
       }
     ]
